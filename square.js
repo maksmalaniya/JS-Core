ed7df82625d031b434b02dc54ea70e8289dad9f5
maksmalaniya <maks.malaniya@gmail.com>
2022-10-11 09:03:42 +0000

    Added

--- square.js
+++ square.js
@@ -1,7 +1,16 @@
-let radius = prompt('Writhe the circle radius below:');
+/* let radius = prompt('Writhe the circle radius below:');
 
 let result = Math.pow((radius * Math.PI), 2);
 
 let square = alert('Your result is ' + result);
 
-square; 
+square; */
+
+document.querySelector('.count').onclick = countSquare;
+
+function countSquare(){
+    let radius = document.querySelector('.radius').value;
+    let result = Math.pow((radius * Math.PI), 2);
+    let answer = 'Your result is ' + result;
+    document.querySelector('.answer').innerHTML = answer;
+}
