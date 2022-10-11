d0f9f93f810c5ac1ccf5250abbeb2924dc344ea0
maksmalaniya <maks.malaniya@gmail.com>
2022-10-11 08:51:59 +0000

    Added

--- Money/money.js
+++ Money/money.js
@@ -1,4 +1,4 @@
-let age = prompt('Напишіть ваш вік:');
+/* let age = prompt('Напишіть ваш вік:');
 
 let lastAge = 65 - age;
 
@@ -8,4 +8,18 @@ let currentYear = neededObj.getFullYear();
 
 let yearAge = currentYear + lastAge;
 
-let newAlert = alert('Час через який Ви вийдете на пенсію (в роках): ' + lastAge + '. Рік, у який Ви вийдете на пенсію: '+ yearAge + '.')
+let newAlert = alert('Час через який Ви вийдете на пенсію (в роках): ' + lastAge + '. Рік, у який Ви вийдете на пенсію: '+ yearAge + '.') */
+
+document.querySelector('.count').onclick = getAnswer;
+
+function getAnswer(){
+    let age = document.querySelector('.age').value;
+    console.log(age)
+    let lastAge = 65 - age;
+    let neededObj = new Date();
+    let currentYear = neededObj.getFullYear();
+    let yearAge = currentYear + lastAge;
+    let message = 'Час через який Ви вийдете на пенсію (в роках): ' + lastAge + '. Рік, у який Ви вийдете на пенсію: '+ yearAge + '.'
+    
+    document.querySelector('.answer').innerHTML = message;
+}
